1bc7dbba79d4342f02301416016190d12d93dc0b
Gaurav <gaurav.bahl@u2opiamobile.com>
2019-12-18 19:33:06 -0500

    Added mysql connector

--- src/main/java/com/example/demo/entities/Amenities.java
+++ src/main/java/com/example/demo/entities/Amenities.java
@@ -1,8 +1,12 @@
 package com.example.demo.entities;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
-@Entity
-@Table(name="Amenities")
-public class Amenities {
+//@Entity
+//@Table(name = "Amenities")
+public class Amenities implements Serializable {
+
+
+
 }
--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -5,15 +5,60 @@ import javax.persistence.*;
 @Entity
 @Table(name="Users")
 public class User {
-    @Column(name="FNAME")
-    private String firstName;
-    @Column(name="LNAME")
-    private String lastName;
-    @Column(name="EMAIL")
-    private String email;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name="FNAME")
+    private String firstName;
+
+    @Column(name="LNAME")
+    private String lastName;
+
+    @Column(name="EMAIL")
+    private String email;
+
     @Column(name="PASSWORD")
     private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
